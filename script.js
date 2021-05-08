69ec878522532ddee1836acee14b459cd2628920
Will Garrett <willgarrett64@yahoo.com>
2021-05-08 12:29:12 +0100

    add more extensive comments and small refactors

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// import {toDoList} from './toDoList.js'
+// Import all drag and drop functionality
+import { dragStart, dragOver, drop } from './dragAndDrop.js'
+
+// Define DOM elements that will be use throughout the script. 
+const toDoListEl = document.getElementById('to-do-list'); //<div> that contains all the to-dos
+const tasksLeftEl = document.getElementById('tasks-left'); //where the number of remaining to-dos is displayed
+const inputEl = document.getElementById("to-do-input"); //where the user writes a new to-do
+const btnEl = document.getElementById('to-do-button'); //button to submit new to-dos
+const clearCompletedEl = document.getElementById('clear-completed'); //button for deleting all completed to-dos
+
+// Define a list of pre-defined to-dos (this will eventually be incorporated into the backend code.)
 let toDoList = [
   {
     text: "Finish CSS on this app",
@@ -17,20 +27,14 @@ let toDoList = [
   },
 ];
 
-import { dragStart, dragOver, drop } from './dragAndDrop.js'
-
-const toDoListElement = document.getElementById('to-do-list');
-const tasksLeftEl = document.getElementById('tasks-left');
-
-  
-
 // Start ID count on one above the amount of items in pre-existing list of to-do items
 let id = toDoList.length + 1;
 let containerId = 1;
 
 
-// create a to-do container element (each to-do item sits in a container div)
+// Create a to-do container element (each to-do item sits in a container div).
 const createContainerEl = (id) => {
+  // create a container <div> element and assign it a class and ID (ID will be generated when rendering) 
   const container = document.createElement('div');
   container.className = "to-do-container";
   container.id = `container ${id}`
@@ -41,13 +45,13 @@ const createContainerEl = (id) => {
 
   return(container)
 }
-// create a new to-do element for the DOM
-const createToDoEl = (toDoObject) => {
-  const toDo = document.createElement('div');
 
+// Create a new to-do element for the DOM by passing in a to-do object from toDoList. 
+const createToDoEl = (toDoObject) => {
+  // create the to-do element's outer <div> and assign it a class and ID
+  const toDo = document.createElement('div');
   toDo.className = "to-do-item";
   toDo.id = toDoObject.id;
-  toDo.active = toDoObject.active;
 
   //create checkbox to mark to-do as complete
   const checkbox = document.createElement('input');
@@ -76,118 +80,125 @@ const createToDoEl = (toDoObject) => {
 
   return(toDo)
 }
-// add a to-do item to the DOM
-const addToDoToDom = (toDo, containerId) => {
-  //first create a container, then add the to-do element as a child element
+
+// Add a to-do item to the DOM. The to-do parameter passed in is the object from toDoList. 
+const addToDoToDom = (toDoObject, containerId) => {
+  //first create a container and the to-do element
   const newContainer = createContainerEl(containerId);
-  newContainer.appendChild(toDo);
-  
+  const newToDo = createToDoEl(toDoObject);
+  //add the to-do as a child of the container
+  newContainer.appendChild(newToDo);
   //add new to-do to the DOM
-  toDoListElement.appendChild(newContainer)
+  toDoListEl.appendChild(newContainer)
 }
 
 
 
-// add a new to-do to the toDoList array
-const addToDo = text => {
+// Add a new to-do to the toDoList array
+const addToDo = input => {
   //create a new toDo object
-  const newToDo = { text: text, active: true, id: id };
+  const newToDo = { text: input, active: true, id: id };
   //increment id so that it's never repeated
   id++;
   //push new object to toDoList array  
   toDoList.push(newToDo);
 }
-// delete a single to-do from the toDoList array
+
+// Delete a single to-do from the toDoList array
 const deleteToDo = id => {
-  // find corresponding to-do in toDoList and remove it
-  const index = toDoList.findIndex(el => el.id == id);
-  toDoList.splice(index, 1);
+  //filter out the element with matching ID from toDoList 
+  toDoList = toDoList.filter(el => el.id != id);
 }
 
-// event handler for adding new to-dos (clicking "Add" or hitting "Enter" key)
+
+// Event handler for adding new to-dos (clicking "Add" or hitting "Enter" key)
 const handleAddNewToDo = e => {
-  if (e.keyCode === 13 || e.type === 'click'){
-  e.preventDefault();
-  const inputEl = document.getElementById("to-do-input");
-  const inputVal = inputEl.value;
-  if (!inputVal) {
-    return;
+  // keyCode 13 is then enter key. 
+  if (e.keyCode === 13 || e.type === 'click') {
+    //prevent default behaviour of submitting a form and refreshing on hitting the button
+    e.preventDefault();
+    //if the input field is blank, do nothing
+    if (!inputEl.value) {
+      return;
+    }
+    //add a new to-do based on user input
+    addToDo(inputEl.value)
+    //re-render the list in the DOM
+    render()
+    //reset inputEl to blank
+    inputEl.value = null;
   }
-
-  addToDo(inputVal)
-  
-  render()
-
-  inputEl.value = null;}
 }
-const btn = document.getElementById('to-do-button');
-btn.addEventListener('click', handleAddNewToDo)
-const input = document.getElementById('to-do-input');
-input.addEventListener('keydown', handleAddNewToDo)
+// Assign event handler to both the button and input elements
+btnEl.addEventListener('click', handleAddNewToDo)
+inputEl.addEventListener('keydown', handleAddNewToDo)
 
-// event handler for deleting a single to-do (by clicking on the cross "x")
+
+// Event handler for deleting a single to-do (by clicking on the cross "x"). This event handler is assigned in createToDoEl function
 const handleDeleteToDo = e => {
+  //access the to-do that was clicked on and its ID
   const toDo = e.target.parentElement;
   const id = toDo.id;
+  //delete the to-do from toDoList based on ID
   deleteToDo(id)
-  // re-render the updated toDoList
+  //re-render the updated toDoList
   render()
 }
 
-// event handler for deleting all completed to-dos
+// Event handler for deleting all completed to-dos
 const handleDeleteAllCompleted = () => {
+  //update toDoList by filtering out all completed (active = false) tasks
   toDoList = toDoList.filter(el => el.active);
-  // re-render the updated toDoList
+  //re-render the updated toDoList
   render()
 }
-const clearCompleted = document.getElementById('clear-completed');
-clearCompleted.addEventListener('click', handleDeleteAllCompleted)
+// Assign event handler to the "clear all completed" element
+clearCompletedEl.addEventListener('click', handleDeleteAllCompleted)
 
-
-// event handler for checkboxes to set to-do as active or complete
+// Event handler for checkboxes to set to-do as active or complete. This event handler is assigned in createToDoEl function
 const handleCheckChange = e => {
+  //access the to-do clicked on, its ID and checkbox value (true or false)
   const value = e.target.checked;
   const id = e.target.parentElement.id;
-  // find corresponding to-do in toDoList and set value of its active property
+  //find corresponding to-do in toDoList and set value of its active property
   const index = toDoList.findIndex(el => el.id == id);
   toDoList[index].active = !value;
-  
+  //re-render the updated toDoList
   render()
 }
 
 
 
 
-// update "view" which determines whether to review all, just completed or just active to-dos in render()
+// Declare and update "view" which determines whether to review all, just completed or just active to-dos in render()
 let view = 'all';
 const setView = e => {
-  // update class so that the selected view is in bold
-  all.className = 'pointer'
-  active.className = 'pointer'
-  completed.className = 'pointer'
-  e.target.className = 'pointer active'
-
-  // update view
+  //remove class of 'active' from currently selected view
+  const active = document.getElementsByClassName('active');
+  active[0].classList.remove('active')
+  //add class of 'active' to newly selected view
+  e.target.className += ' active';
+  //update view
   view = e.target.id;
+  //re-render list of to-dos 
   render();
 }
-// assign setView event listener to the all, active and completed elements. 
-const all = document.getElementById('all');
-const active = document.getElementById('active');
-const completed = document.getElementById('completed');
-all.onclick = setView;
-active.onclick = setView;
-completed.onclick = setView;
+// Assign setView event listener to the all, active and completed elements (all have a class of 'view'). 
+const viewSelectors = document.querySelectorAll('.view')
+viewSelectors.forEach(el => {
+  el.onclick = setView;
+})
 
 
-// render all to-dos from the toDoList array
+/* Render all to-dos from the toDoList array (dependent on 'view').
+Notes: This is an early draft of a function to render elements to the DOM when toDoList is updated. As it stands, it's fairly inefficient as it deletes all to-dos from the DOM and re-renders them all, however I plan to expand on this to compare the current DOM with the new DOM to only render what has changed, similar to how React works.
+*/
 const render = () => {
-  // remove all elements currently in the to-do list <div>
-  while (toDoListElement.lastChild) {
-    toDoListElement.lastChild.remove();
+  //remove all elements currently in the to-do list <div>
+  while (toDoListEl.lastChild) {
+    toDoListEl.lastChild.remove();
   }
-
-  // create an array of elements to display (based on view all, active or completed)
+  //create a new array of elements to display (based on view all, active or completed)
   let toDisplay = [];
   switch (view) {
     case 'all':
@@ -200,16 +211,15 @@ const render = () => {
       toDisplay = toDoList.filter(el => !el.active);
       break;
   }
-
-  //re-set container ID to 1. This will increment for each to-ado added
+  //reset container ID to 1. This will increment for each to-do added
   containerId = 1; 
-  // render toDisplay into the to-do list <div>
+  //render each item of toDisplay to the to-do list <div>
   toDisplay.forEach(toDo => {
-    addToDoToDom(createToDoEl(toDo), containerId)
+    addToDoToDom(toDo, containerId)
     containerId++;
   })
   
-  // update the tasks-left element, counting all "active" to-dos
+  //update the tasks-left element, counting all "active" to-dos - I plan to improve this section later
   let tasksLeft = 0;
   toDoList.forEach(el => {
     if (el.active) {
@@ -219,4 +229,5 @@ const render = () => {
   tasksLeftEl.innerHTML = `${tasksLeft} items left`;
 }
 
+// Call render once on first page load. It will then be re-called on each change
 render()
